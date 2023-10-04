51b0567e6d64fd9c60e4fcc90b2eafd17e870b39
Duncan Lester <lesterduncan99@gmail.com>
2023-10-04 10:45:08 -0700

    basic routing acquired

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const app = express()
 const port = 3000
+app.use(express.static('resources'))
+app.set('view engine', 'ejs')
 
-app.get('/', (req,res) => {
-    //res.render(//view//)
+app.get('/state', (req,res) => {
+    res.render('state')
+})
+app.get('/city1', (req,res) => {
+    res.render('city1')
+})
+app.get('/city2', (req,res) => {
+    res.render('city2')
+})
+app.get('city3', (req,res) => {
+    res.render('city3')
 })
 
-//app.listen(port, () =>{
-    //junk
-//})
+app.listen(port, function (req,res) {
+    console.log('listening on port', port);
+})
